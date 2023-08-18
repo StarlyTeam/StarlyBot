df481bb9ef2e4b5f2a9b62003fef38029f708534
idkNicks <yangdaeyeong0808@gmail.com>
2023-08-19 04:07:26 +0900

    feat: clean code

--- src/main/java/kr/starly/discordbot/command/impl/CleanUpCommand.java
+++ src/main/java/kr/starly/discordbot/command/impl/CleanUpCommand.java
@@ -4,6 +4,7 @@ import kr.starly.discordbot.command.BotCommand;
 import kr.starly.discordbot.command.DiscordCommand;
 import kr.starly.discordbot.configuration.ConfigManager;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
@@ -26,34 +27,37 @@ public class CleanUpCommand extends DiscordCommand {
 
         String[] args = event.getMessage().getContentRaw().split("\\s", 2);
         if (args.length < 2) {
-            EmbedBuilder embed = new EmbedBuilder()
+            MessageEmbed embed = new EmbedBuilder()
                     .setColor(Color.decode(EMBED_COLOR_ERROR))
                     .setTitle("<a:loading:1141623256558866482> 오류 | 잘못된 입력 <a:loading:1141623256558866482>")
-                    .setDescription("**청소할 메시지의 개수를 입력해주세요. 예) ?청소 10**");
+                    .setDescription("**청소할 메시지의 개수를 입력해주세요. 예) ?청소 10**")
+                    .build();
 
-            event.getChannel().sendMessageEmbeds(embed.build()).queue();
+            event.getChannel().sendMessageEmbeds(embed).queue();
             return;
         }
 
         try {
             int count = Integer.parseInt(args[1]);
             if (count < 1 || count > 99) {
-                EmbedBuilder embed = new EmbedBuilder()
+                MessageEmbed embed = new EmbedBuilder()
                         .setColor(Color.decode(EMBED_COLOR_ERROR))
                         .setTitle("<a:loading:1141623256558866482> 오류 | 잘못된 입력 <a:loading:1141623256558866482>")
-                        .setDescription("**메시지는 1개에서 99개까지만 삭제할 수 있습니다.**");
+                        .setDescription("**메시지는 1개에서 99개까지만 삭제할 수 있습니다.**")
+                        .build();
 
-                event.getChannel().sendMessageEmbeds(embed.build()).queue();
+                event.getChannel().sendMessageEmbeds(embed).queue();
             } else {
                 deleteMessages(event.getChannel(), count);
             }
         } catch (NumberFormatException e) {
-            EmbedBuilder embed = new EmbedBuilder()
+            MessageEmbed embed = new EmbedBuilder()
                     .setColor(Color.decode(EMBED_COLOR_ERROR))
                     .setTitle("<a:loading:1141623256558866482> 오류 | 잘못된 입력 <a:loading:1141623256558866482>")
-                    .setDescription("**유효한 숫자를 입력해주세요. 예) ?청소 10**");
+                    .setDescription("**유효한 숫자를 입력해주세요. 예) ?청소 10**")
+                    .build();
 
-            event.getChannel().sendMessageEmbeds(embed.build()).queue();
+            event.getChannel().sendMessageEmbeds(embed).queue();
         }
     }
 
@@ -61,11 +65,12 @@ public class CleanUpCommand extends DiscordCommand {
         channel.getIterableHistory()
                 .takeAsync(count + 1)
                 .thenAccept(channel::purgeMessages);
-        EmbedBuilder embed = new EmbedBuilder()
+        MessageEmbed embed = new EmbedBuilder()
                 .setColor(Color.decode(EMBED_COLOR_SUCCESS))
                 .setTitle("<a:success:1141625729386287206> 성공 | 채팅청소 <a:success:1141625729386287206>")
-                .setDescription("**" + count + "개의 메시지를 청소했습니다.**");
+                .setDescription("**" + count + "개의 메시지를 청소했습니다.**")
+                .build();
 
-        channel.sendMessageEmbeds(embed.build()).queue();
+        channel.sendMessageEmbeds(embed).queue();
     }
 }
--- src/main/java/kr/starly/discordbot/command/impl/CreateVerifyCommand.java
+++ src/main/java/kr/starly/discordbot/command/impl/CreateVerifyCommand.java
@@ -4,6 +4,7 @@ import kr.starly.discordbot.command.BotCommand;
 import kr.starly.discordbot.command.DiscordCommand;
 import kr.starly.discordbot.configuration.ConfigManager;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
@@ -25,7 +26,7 @@ public class CreateVerifyCommand extends DiscordCommand {
 
         event.getMessage().delete().queue();
 
-        EmbedBuilder verifyEmbed = new EmbedBuilder()
+        MessageEmbed verifyEmbed = new EmbedBuilder()
                 .setColor(Color.decode(EMBED_COLOR))
                 .setTitle("<a:success:1141625729386287206> 인증 | 스탈리 커뮤니티 <a:success:1141625729386287206>")
                 .setDescription("> **스탈리 커뮤니티에 오신걸 환영합니다.** \n" +
@@ -34,9 +35,9 @@ public class CreateVerifyCommand extends DiscordCommand {
                 )
                 .addField("> <:notice:1141720944935719002> 필독 사항",
                         "**> 이용약관을 필독해주세요.**\n" +
-                                "**> 서버규칙을 *필독해주세요.**\n" +
+                                "**> 서버규칙을 필독해주세요.**\n" +
                                 "**> 불법적인 서버에 활동 중이라면 지금 당장 퇴장해주세요.**\n" +
-                                "**> 자신의 디스코드 닉네임 또는 아이디가 부적절한지 확인해주세요. **\n \u1CBB", true)
+                                "**> 자신의 디스코드 닉네임 또는 아이디가 부적절한지 확인해주세요. **\u1CBB", true)
                 .addField("> <a:loading:1141623256558866482> 인증 오류",
                         "**> 자신의 디스코드 계정에 전화번호가 추가되어있는지 확인해보세요.**\n" +
                         "**> 30일 이내에 생성된 계정인지 확인해보세요.**\n" +
@@ -45,12 +46,13 @@ public class CreateVerifyCommand extends DiscordCommand {
                         "**> 3일 이내에서 서버를 퇴장할 경우 다운로드 금지가 됩니다.**\n" +
                                 "**> 부계정 사용을 금지합니다. **\n", true)
                 .setThumbnail("https://media.discordapp.net/attachments/1059420652722999386/1141710970859835423/KakaoTalk_20230725_014437871_01.png?width=569&height=569")
-                .setFooter("인증을 완료할 경우 위 내용을 모두 동의한 것으로 간주됩니다.", "https://media.discordapp.net/attachments/1059420652722999386/1141710970859835423/KakaoTalk_20230725_014437871_01.png?width=569&height=569");
+                .setFooter("인증을 완료할 경우 위 내용을 모두 동의한 것으로 간주됩니다.", "https://media.discordapp.net/attachments/1059420652722999386/1141710970859835423/KakaoTalk_20230725_014437871_01.png?width=569&height=569")
+                .build();
 
         Button verify = Button.success("successVerify", "인증하기");
         Button help = Button.danger("helpVerify", "인증이 안되시나요?");
         Button termsOfService = Button.primary("termsOfService", "이용약관");
         Button serverRule = Button.primary("serverRule", "서버규칙");
-        event.getChannel().sendMessageEmbeds(verifyEmbed.build()).addActionRow(verify, help, termsOfService, serverRule).queue();
+        event.getChannel().sendMessageEmbeds(verifyEmbed).addActionRow(verify, help, termsOfService, serverRule).queue();
     }
 }
