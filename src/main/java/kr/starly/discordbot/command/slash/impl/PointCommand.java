92a8e573ea51f71d457424b807e1af05fd81ba78
yejunho10 <hyjcompany30@naver.com>
2023-10-02 02:28:43 +0900

    refactor: styled code

--- src/main/java/kr/starly/discordbot/command/slash/impl/PointCommand.java
+++ src/main/java/kr/starly/discordbot/command/slash/impl/PointCommand.java
@@ -9,6 +9,7 @@ import kr.starly.discordbot.util.PermissionUtil;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -24,42 +25,42 @@ import java.util.concurrent.atomic.AtomicInteger;
                 @BotSlashCommand.SubCommand(
                         name = "지급",
                         description = "포인트를 지급합니다.",
-                        names = {"유저", "포인트"},
+                        optionName = {"유저", "포인트"},
                         optionType = {OptionType.USER, OptionType.INTEGER},
                         optionDescription = {"유저를 선택하세요.", "지급할 포인트를 입력하세요."},
-                        required = {true, true}
+                        optionRequired = {true, true}
                 ),
                 @BotSlashCommand.SubCommand(
                         name = "제거",
                         description = "포인트를 제거합니다.",
-                        names = {"유저", "포인트"},
+                        optionName = {"유저", "포인트"},
                         optionType = {OptionType.USER, OptionType.INTEGER},
                         optionDescription = {"유저를 선택하세요.", "제거할 포인트를 입력하세요."},
-                        required = {true, true}
+                        optionRequired = {true, true}
                 ),
                 @BotSlashCommand.SubCommand(
                         name = "설정",
                         description = "포인트를 설정합니다.",
-                        names = {"유저", "포인트"},
+                        optionName = {"유저", "포인트"},
                         optionType = {OptionType.USER, OptionType.INTEGER},
                         optionDescription = {"유저를 선택하세요.", "설정할 포인트를 입력하세요."},
-                        required = {true, true}
+                        optionRequired = {true, true}
                 ),
                 @BotSlashCommand.SubCommand(
                         name = "확인",
                         description = "포인트를 확인합니다.",
-                        names = {"유저"},
+                        optionName = {"유저"},
                         optionType = {OptionType.USER},
                         optionDescription = {"확인할 유저를 선택하세요. (선택하지 않으면 자신의 포인트를 확인합니다.)"},
-                        required = {false}
+                        optionRequired = {false}
                 ),
                 @BotSlashCommand.SubCommand(
                         name = "초기화",
                         description = "포인트를 초기화합니다.",
-                        names = {"유저"},
+                        optionName = {"유저"},
                         optionType = {OptionType.USER},
                         optionDescription = {"유저를 선택하세요."},
-                        required = {true}
+                        optionRequired = {true}
                 ),
                 @BotSlashCommand.SubCommand(
                         name = "순위",
@@ -91,57 +92,56 @@ public class PointCommand implements DiscordSlashCommand {
 
         switch (subCommand) {
             case "지급" -> {
-                String userIdForAdd = event.getOption("유저").getAsUser().getId();
-                String userAvatarForAdd = event.getOption("유저").getAsUser().getAvatarUrl();
+                User target = event.getOption("유저").getAsUser();
                 int pointToAdd = getSafeIntFromOption(event.getOption("포인트"));
-                DatabaseConfig.getUserInfoService().addPoint(userIdForAdd, pointToAdd);
+                DatabaseConfig.getUserInfoService().addPoint(target.getId(), pointToAdd);
                 messageEmbed = new EmbedBuilder()
                         .setColor(Color.decode(EMBED_COLOR_SUCCESS))
                         .setTitle("<a:success:1141625729386287206> 지급 완료 | 포인트 <a:success:1141625729386287206>")
-                        .setDescription("> **<@" + userIdForAdd + ">님에게 " + pointToAdd + "포인트를 지급하였습니다.**")
-                        .setThumbnail(userAvatarForAdd)
+                        .setDescription("> **" + target.getAsMention() + "님에게 " + pointToAdd + "포인트를 지급하였습니다.**")
+                        .setThumbnail(target.getAvatarUrl())
                         .build();
                 event.replyEmbeds(messageEmbed).queue();
             }
+
             case "제거" -> {
-                String userIdForRemove = event.getOption("유저").getAsUser().getId();
-                String userAvatarForRemove = event.getOption("유저").getAsUser().getAvatarUrl();
+                User target = event.getOption("유저").getAsUser();
                 int pointToRemove = getSafeIntFromOption(event.getOption("포인트"));
-                DatabaseConfig.getUserInfoService().removePoint(userIdForRemove, pointToRemove);
+                DatabaseConfig.getUserInfoService().removePoint(target.getId(), pointToRemove);
                 messageEmbed = new EmbedBuilder()
                         .setColor(Color.decode(EMBED_COLOR_ERROR))
                         .setTitle("<a:success:1141625729386287206> 제거 완료 | 포인트 <a:success:1141625729386287206>")
-                        .setDescription("> **<@" + userIdForRemove + ">님의 " + pointToRemove + "포인트를 제거하였습니다.**")
-                        .setThumbnail(userAvatarForRemove)
+                        .setDescription("> **" + target.getAsMention() + "님의 " + pointToRemove + "포인트를 제거하였습니다.**")
+                        .setThumbnail(target.getAvatarUrl())
                         .build();
                 event.replyEmbeds(messageEmbed).queue();
             }
+
             case "설정" -> {
-                String userIdForSet = event.getOption("유저").getAsUser().getId();
-                String userAvatarForSet = event.getOption("유저").getAsUser().getAvatarUrl();
+                User target = event.getOption("유저").getAsUser();
                 int pointToSet = getSafeIntFromOption(event.getOption("포인트"));
-                DatabaseConfig.getUserInfoService().setPoint(userIdForSet, pointToSet);
+                DatabaseConfig.getUserInfoService().setPoint(target.getId(), pointToSet);
                 messageEmbed = new EmbedBuilder()
                         .setColor(Color.decode(EMBED_COLOR))
                         .setTitle("<a:success:1141625729386287206> 설정 완료 | 포인트 <a:success:1141625729386287206>")
-                        .setDescription("> **<@" + userIdForSet + ">님의 포인트를 " + pointToSet + "로 설정되었습니다.**")
-                        .setThumbnail(userAvatarForSet)
+                        .setDescription("> **" + target.getAsMention() + "님의 포인트를 " + pointToSet + "로 설정되었습니다.**")
+                        .setThumbnail(target.getAvatarUrl())
                         .build();
                 event.replyEmbeds(messageEmbed).queue();
             }
-            case "확인" -> handleCheckCommand(event);
+
             case "초기화" -> {
-                String userIdForReset = event.getOption("유저").getAsUser().getId();
-                String userAvatarReset = event.getOption("유저").getAsUser().getAvatarUrl();
-                DatabaseConfig.getUserInfoService().setPoint(userIdForReset, 0);
+                User target = event.getOption("유저").getAsUser();
+                DatabaseConfig.getUserInfoService().setPoint(target.getId(), 0);
                 messageEmbed = new EmbedBuilder()
                         .setColor(Color.decode(EMBED_COLOR_SUCCESS))
                         .setTitle("<a:success:1141625729386287206> 초기화 완료 | 포인트 <a:success:1141625729386287206>")
-                        .setDescription("> **<@" + userIdForReset + ">님의 포인트를 초기화하였습니다.**")
-                        .setThumbnail(userAvatarReset)
+                        .setDescription("> **" + target.getAsMention() + "님의 포인트를 초기화하였습니다.**")
+                        .setThumbnail(target.getAvatarUrl())
                         .build();
                 event.replyEmbeds(messageEmbed).queue();
             }
+
             case "순위" -> {
                 List<UserInfo> topUsers = DatabaseConfig.getUserInfoService().getTopUsersByPoints(10);
                 StringBuilder rankMessage = new StringBuilder();
@@ -151,7 +151,8 @@ public class PointCommand implements DiscordSlashCommand {
                     event.getJDA().retrieveUserById(user.discordId()).queue(retrievedUser -> {
                         if (retrievedUser != null) {
                             rankMessage.append("> ")
-                                    .append("**" + (processedUsers.incrementAndGet()))
+                                    .append("**")
+                                    .append(processedUsers.incrementAndGet())
                                     .append(".")
                                     .append(" <@" + user.discordId() + "> ")
                                     .append(": ")
@@ -171,6 +172,7 @@ public class PointCommand implements DiscordSlashCommand {
                     });
                 }
             }
+
             default -> {
                 messageEmbed = new EmbedBuilder()
                         .setColor(Color.decode(EMBED_COLOR_ERROR))
