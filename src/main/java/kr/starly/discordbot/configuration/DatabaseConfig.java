7bf8f279bddba558a407fdccc8f9694ad75a1000
yejunho10 <hyjcompany30@naver.com>
2023-10-02 16:38:01 +0900

    refactor: styled code

--- src/main/java/kr/starly/discordbot/configuration/DatabaseConfig.java
+++ src/main/java/kr/starly/discordbot/configuration/DatabaseConfig.java
@@ -9,7 +9,6 @@ import kr.starly.discordbot.repository.WarnRepository;
 import kr.starly.discordbot.repository.impl.MongoTicketRepository;
 import kr.starly.discordbot.repository.impl.MongoUserInfoRepository;
 import kr.starly.discordbot.repository.UserInfoRepository;
-
 import kr.starly.discordbot.repository.impl.MongoWarnRepository;
 import kr.starly.discordbot.service.TicketInfoService;
 import kr.starly.discordbot.service.UserInfoService;
@@ -25,32 +24,28 @@ public class DatabaseConfig {
     private static final String DB_HOST = configProvider.getString("DB_HOST");
     private static final String DB_DATABASE = configProvider.getString("DB_DATABASE");
     private static final String DB_COLLECTION_USER = configProvider.getString("DB_COLLECTION_USER");
-
     private static final String DB_COLLECTION_TICKET = configProvider.getString("DB_COLLECTION_TICKET");
-
     private static final String DB_COLLECTION_WARN = configProvider.getString("DB_COLLECTION_WARN");
 
     private static final MongoClient mongoClient = MongoClients.create(DB_HOST);
-
     private static final MongoDatabase database = mongoClient.getDatabase(DB_DATABASE);
-    private static final MongoCollection<Document> userCollection = database.getCollection(DB_COLLECTION_USER);
 
+    private static final MongoCollection<Document> userCollection = database.getCollection(DB_COLLECTION_USER);
+    private static final MongoCollection<Document> ticketCollection = database.getCollection(DB_COLLECTION_TICKET);
     private static final MongoCollection<Document> warnCollection = database.getCollection(DB_COLLECTION_WARN);
 
-    private static final MongoCollection<Document> ticketCollection = database.getCollection(DB_COLLECTION_TICKET);
 
     private static final UserInfoRepository userInfoRepository = new MongoUserInfoRepository(userCollection);
-
     private static final TicketRepository ticketRepository = new MongoTicketRepository(ticketCollection);
-
     private static final WarnRepository warnRepository = new MongoWarnRepository(warnCollection);
 
-    @Getter
-    private static final TicketInfoService ticketInfoService = new TicketInfoService(ticketRepository);
 
     @Getter
     private static final UserInfoService userInfoService = new UserInfoService(userInfoRepository);
 
     @Getter
     private static final WarnService warnService = new WarnService(warnRepository);
+    @Getter private static final UserInfoService userInfoService = new UserInfoService(userInfoRepository);
+    @Getter private static final TicketInfoService ticketInfoService = new TicketInfoService(ticketRepository);
+    @Getter private static final WarnService warnService = new WarnService(warnRepository);
 }
