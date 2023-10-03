86c84297c21e751d6d29a6134addc4a23c4b7f7e
yejunho10 <hyjcompany30@naver.com>
2023-10-04 01:49:28 +0900

    refactor: `multi-line` text to `text block`

--- src/main/java/kr/starly/discordbot/command/impl/CreatePluginManagementCommand.java
+++ src/main/java/kr/starly/discordbot/command/impl/CreatePluginManagementCommand.java
@@ -33,15 +33,17 @@ public class CreatePluginManagementCommand implements DiscordCommand {
         MessageEmbed verifyEmbed = new EmbedBuilder()
                 .setColor(EMBED_COLOR)
                 .setTitle("<a:loading:1141623256558866482> 플러그인 관리 | 스탈리 (관리자 전용) <a:loading:1141623256558866482>")
-                .setDescription("> **플러그인 관리 메뉴입니다.** \n" +
-                        "> **아래 선택 메뉴에서 원하는 작업을 선택하세요.** \n\n" +
-                        "─────────────────────────────────────────────────"
+                .setDescription("""
+                        > **플러그인 관리 메뉴입니다.**\s
+                        > **아래 선택 메뉴에서 원하는 작업을 선택하세요.**\s
+
+                        ─────────────────────────────────────────────────"""
                 )
                 .setThumbnail("https://imagedelivery.net/zI1a4o7oosLEca8Wq4ML6w/e7a1b4a6-854c-499b-5bb2-5737af369900/public")
                 .setFooter("이 기능은 관리자 전용입니다.", "https://imagedelivery.net/zI1a4o7oosLEca8Wq4ML6w/e7a1b4a6-854c-499b-5bb2-5737af369900/public")
                 .build();
 
-        StringSelectMenu pluginSelectionMenu = StringSelectMenu.create("plugin-management")
+        StringSelectMenu pluginSelectionMenu = StringSelectMenu.create("plugin-management-action")
                 .setPlaceholder("원하시는 작업을 선택해 주세요.")
                 .addOption("플러그인 등록", "plugin-register", "플러그인을 등록합니다.", Emoji.fromUnicode("➕"))
                 .addOption("플러그인 삭제", "plugin-delete", "플러그인을 삭제합니다.", Emoji.fromUnicode("❌"))
