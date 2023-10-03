485d150e266025c6423a3a1b98cb964746d4e5e6
yejunho10 <hyjcompany30@naver.com>
2023-10-04 01:50:07 +0900

    feat: extract functions to `CFImagesUtil`

--- src/main/java/kr/starly/discordbot/command/slash/impl/ImageUploadCommand.java
+++ src/main/java/kr/starly/discordbot/command/slash/impl/ImageUploadCommand.java
@@ -2,10 +2,10 @@ package kr.starly.discordbot.command.slash.impl;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import kr.starly.discordbot.command.slash.BotSlashCommand;
 import kr.starly.discordbot.command.slash.DiscordSlashCommand;
 import kr.starly.discordbot.configuration.ConfigProvider;
+import kr.starly.discordbot.util.CFImagesUtil;
 import kr.starly.discordbot.util.PermissionUtil;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
@@ -14,12 +14,8 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 
-import javax.net.ssl.HttpsURLConnection;
 import java.awt.*;
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.stream.Collectors;
 
 @BotSlashCommand(
         command = "이미지",
@@ -50,8 +46,6 @@ import java.util.stream.Collectors;
 public class ImageUploadCommand implements DiscordSlashCommand {
 
     private final ConfigProvider configProvider = ConfigProvider.getInstance();
-    private final String ACCOUNT_ID = configProvider.getString("ACCOUNT_ID");
-    private final String API_TOKEN = configProvider.getString("API_TOKEN");
     private final Color EMBED_COLOR = Color.decode(configProvider.getString("EMBED_COLOR"));
     private final Color EMBED_COLOR_SUCCESS = Color.decode(configProvider.getString("EMBED_COLOR_SUCCESS"));
     private final Color EMBED_COLOR_ERROR = Color.decode(configProvider.getString("EMBED_COLOR_ERROR"));
@@ -60,6 +54,7 @@ public class ImageUploadCommand implements DiscordSlashCommand {
     public void execute(SlashCommandInteractionEvent event) {
         if (!PermissionUtil.hasPermission(event.getMember(), Permission.ADMINISTRATOR)) {
             PermissionUtil.sendPermissionError(event.getChannel());
+            return;
         }
 
         String subCommand = event.getSubcommandName();
@@ -75,63 +70,9 @@ public class ImageUploadCommand implements DiscordSlashCommand {
                     event.replyEmbeds(messageEmbed).queue();
                 }
 
+                String imageUrl;
                 try {
-                    final String boundary = "A1B2C3";
-                    final String crlf = "\r\n";
-
-                    URL url = new URL("https://api.cloudflare.com/client/v4/accounts/" + ACCOUNT_ID + "/images/v1");
-
-                    HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-                    conn.setDoOutput(true);
-                    conn.setDoInput(true);
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-                    conn.setRequestProperty("Authorization", "Bearer " + API_TOKEN);
-
-                    OutputStream httpConnOutputStream = conn.getOutputStream();
-                    DataOutputStream request = new DataOutputStream(httpConnOutputStream);
-
-                    request.writeBytes("--" + boundary + crlf);
-                    request.writeBytes("Content-Disposition: form-data; name=\"url\"" + crlf);
-                    request.writeBytes(crlf);
-                    request.writeBytes(image.getUrl() + crlf);
-                    request.writeBytes("--" + boundary + "--");
-                    request.flush();
-                    request.close();
-
-                    int status = conn.getResponseCode();
-                    if (status == HttpsURLConnection.HTTP_OK) {
-                        InputStream is;
-                        if (conn.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
-                            is = conn.getInputStream();
-                        } else {
-                            is = conn.getErrorStream();
-                        }
-
-                        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                        JsonObject jsonObject = JsonParser.parseString(br.lines().collect(Collectors.joining(""))).getAsJsonObject();
-
-                        JsonArray errors = jsonObject.get("errors").getAsJsonArray();
-                        if (!errors.isEmpty()) {
-                            MessageEmbed messageEmbed = new EmbedBuilder()
-                                    .setColor(EMBED_COLOR_ERROR)
-                                    .setTitle("<a:loading:1141623256558866482> 오류 | 요청 실패 <a:loading:1141623256558866482>")
-                                    .setDescription("**이미지를 업로드하지 못했습니다.**")
-                                    .build();
-                            event.replyEmbeds(messageEmbed).queue();
-                            return;
-                        }
-
-                        JsonObject result = jsonObject.get("result").getAsJsonObject();
-                        JsonArray variants = result.getAsJsonArray("variants");
-
-                        MessageEmbed messageEmbed = new EmbedBuilder()
-                                .setColor(EMBED_COLOR_SUCCESS)
-                                .setTitle("<a:success:1141625729386287206> 업로드 완료 | 이미지 <a:success:1141625729386287206>")
-                                .setDescription("**성공적으로 이미지 업로드했습니다.**\n\n```" + variants.get(0).getAsString() + "```")
-                                .build();
-                        event.replyEmbeds(messageEmbed).queue();
-                    }
+                    imageUrl = CFImagesUtil.uploadImage(image.getUrl());
                 } catch (IOException ex) {
                     ex.printStackTrace();
 
@@ -141,122 +82,92 @@ public class ImageUploadCommand implements DiscordSlashCommand {
                             .setDescription("**이미지를 업로드하지 못했습니다.**")
                             .build();
                     event.replyEmbeds(messageEmbed).queue();
+                    return;
                 }
+
+                if (imageUrl == null) {
+                    MessageEmbed messageEmbed = new EmbedBuilder()
+                            .setColor(EMBED_COLOR_ERROR)
+                            .setTitle("<a:loading:1141623256558866482> 오류 | 요청 실패 <a:loading:1141623256558866482>")
+                            .setDescription("**이미지를 업로드하지 못했습니다.**")
+                            .build();
+                    event.replyEmbeds(messageEmbed).queue();
+                    return;
+                }
+
+                MessageEmbed messageEmbed = new EmbedBuilder()
+                        .setColor(EMBED_COLOR_SUCCESS)
+                        .setTitle("<a:success:1141625729386287206> 업로드 완료 | 이미지 <a:success:1141625729386287206>")
+                        .setDescription("**성공적으로 이미지 업로드했습니다.**\n\n```" + imageUrl + "```")
+                        .build();
+                event.replyEmbeds(messageEmbed).queue();
             }
 
             case "삭제" -> {
                 String imageId = event.getOption("아이디").getAsString();
 
                 try {
-                    URL url = new URL("https://api.cloudflare.com/client/v4/accounts/" + ACCOUNT_ID + "/images/v1/" + imageId);
+                    CFImagesUtil.deleteImage(imageId);
 
-                    HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-                    conn.setDoInput(true);
-                    conn.setRequestMethod("DELETE");
-                    conn.setRequestProperty("Authorization", "Bearer " + API_TOKEN);
-
-                    int status = conn.getResponseCode();
-                    if (status == HttpsURLConnection.HTTP_OK) {
-                        InputStream is;
-                        if (conn.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
-                            is = conn.getInputStream();
-                        } else {
-                            is = conn.getErrorStream();
-                        }
-
-                        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                        JsonObject jsonObject = JsonParser.parseString(br.lines().collect(Collectors.joining(""))).getAsJsonObject();
-
-                        JsonArray errors = jsonObject.get("errors").getAsJsonArray();
-                        if (!errors.isEmpty()) {
-                            MessageEmbed messageEmbed = new EmbedBuilder()
-                                    .setColor(EMBED_COLOR_ERROR)
-                                    .setTitle("<a:loading:1141623256558866482> 오류 | 요청 실패 <a:loading:1141623256558866482>")
-                                    .setDescription("**이미지를 삭제하지 못했습니다.**")
-                                    .build();
-                            event.replyEmbeds(messageEmbed).queue();
-                            return;
-                        }
-
-                        MessageEmbed messageEmbed = new EmbedBuilder()
-                                .setColor(EMBED_COLOR_SUCCESS)
-                                .setTitle("<a:success:1141625729386287206> 삭제 완료 | 이미지 <a:success:1141625729386287206>")
-                                .setDescription("**성공적으로 이미지를 삭제했습니다.**")
-                                .build();
-                        event.replyEmbeds(messageEmbed).queue();
-                    }
+                    MessageEmbed messageEmbed = new EmbedBuilder()
+                            .setColor(EMBED_COLOR_SUCCESS)
+                            .setTitle("<a:success:1141625729386287206> 삭제 완료 | 이미지 <a:success:1141625729386287206>")
+                            .setDescription("**성공적으로 이미지를 삭제했습니다.**")
+                            .build();
+                    event.replyEmbeds(messageEmbed).queue();
                 } catch (IOException ex) {
                     ex.printStackTrace();
 
                     MessageEmbed messageEmbed = new EmbedBuilder()
                             .setColor(EMBED_COLOR_ERROR)
                             .setTitle("<a:loading:1141623256558866482> 오류 | 내부 프로세스 <a:loading:1141623256558866482>")
-                            .setDescription("**이미지를 삭제하지 못했습니다.**")
+                            .setDescription("**이미지를 삭제하지 못 했습니다.**")
                             .build();
                     event.replyEmbeds(messageEmbed).queue();
                 }
             }
 
             case "목록" -> {
+                JsonArray images;
                 try {
-                    URL url = new URL("https://api.cloudflare.com/client/v4/accounts/" + ACCOUNT_ID + "/images/v2");
-
-                    HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-                    conn.setDoInput(true);
-                    conn.setRequestMethod("GET");
-                    conn.setRequestProperty("Authorization", "Bearer " + API_TOKEN);
-
-                    int status = conn.getResponseCode();
-                    if (status == HttpsURLConnection.HTTP_OK) {
-                        InputStream is;
-                        if (conn.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
-                            is = conn.getInputStream();
-                        } else {
-                            is = conn.getErrorStream();
-                        }
-
-                        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                        JsonObject jsonObject = JsonParser.parseString(br.lines().collect(Collectors.joining(""))).getAsJsonObject();
-
-                        JsonArray errors = jsonObject.get("errors").getAsJsonArray();
-                        if (!errors.isEmpty()) {
-                            MessageEmbed messageEmbed = new EmbedBuilder()
-                                    .setColor(EMBED_COLOR_ERROR)
-                                    .setTitle("<a:loading:1141623256558866482> 오류 | 요청 실패 <a:loading:1141623256558866482>")
-                                    .setDescription("**이미지 목록을 불러오지 못했습니다.**")
-                                    .build();
-                            event.replyEmbeds(messageEmbed).queue();
-                            return;
-                        }
-
-                        JsonArray images = jsonObject.get("result").getAsJsonObject().get("images").getAsJsonArray();
-
-                        StringBuilder sb = new StringBuilder();
-                        images.forEach(image -> {
-                            JsonObject imageData = image.getAsJsonObject();
-                            String filename = imageData.get("filename").getAsString();
-                            String id = imageData.get("id").getAsString();
-
-                            sb.append(id + " (" + filename + ")\n");
-                        });
-
-                        MessageEmbed messageEmbed = new EmbedBuilder()
-                                .setColor(EMBED_COLOR)
-                                .setTitle("<a:loading:1141623256558866482> 목록 | 이미지 <a:loading:1141623256558866482>")
-                                .setDescription("```" + sb + "```")
-                                .build();
-                        event.replyEmbeds(messageEmbed).queue();
-                    }
+                    images = CFImagesUtil.listImage();
                 } catch (IOException ex) {
                     ex.printStackTrace();
 
                     MessageEmbed messageEmbed = new EmbedBuilder()
                             .setColor(EMBED_COLOR_ERROR)
                             .setTitle("<a:loading:1141623256558866482> 오류 | 내부 프로세스 <a:loading:1141623256558866482>")
-                            .setDescription("**이미지 목록을 불러오지 못했습니다.**")
+                            .setDescription("**이미지 목록을 불러오지 못 했습니다.**")
                             .build();
                     event.replyEmbeds(messageEmbed).queue();
+                    return;
                 }
+
+                if (images == null) {
+                    MessageEmbed messageEmbed = new EmbedBuilder()
+                            .setColor(EMBED_COLOR_ERROR)
+                            .setTitle("<a:loading:1141623256558866482> 오류 | 내부 프로세스 <a:loading:1141623256558866482>")
+                            .setDescription("**이미지 목록을 불러오지 못 했습니다.**")
+                            .build();
+                    event.replyEmbeds(messageEmbed).queue();
+                    return;
+                }
+
+                StringBuilder sb = new StringBuilder();
+                images.forEach(image -> {
+                    JsonObject imageData = image.getAsJsonObject();
+                    String filename = imageData.get("filename").getAsString();
+                    String id = imageData.get("id").getAsString();
+
+                    sb.append(id + " (" + filename + ")\n");
+                });
+
+                MessageEmbed messageEmbed = new EmbedBuilder()
+                        .setColor(EMBED_COLOR)
+                        .setTitle("<a:loading:1141623256558866482> 목록 | 이미지 <a:loading:1141623256558866482>")
+                        .setDescription("```" + sb + "```")
+                        .build();
+                event.replyEmbeds(messageEmbed).queue();
             }
         }
     }
---
+++ src/main/java/kr/starly/discordbot/util/CFImagesUtil.java
@@ -0,0 +1,120 @@
+package kr.starly.discordbot.util;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import kr.starly.discordbot.configuration.ConfigProvider;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+public class CFImagesUtil {
+
+    private static final ConfigProvider configProvider = ConfigProvider.getInstance();
+    private static final String ACCOUNT_ID = configProvider.getString("ACCOUNT_ID");
+    private static final String API_TOKEN = configProvider.getString("API_TOKEN");
+
+    public static String uploadImage(String imageUrl) throws IOException {
+        final String boundary = "A1B2C3";
+        final String crlf = "\r\n";
+
+        URL url = new URL("https://api.cloudflare.com/client/v4/accounts/" + ACCOUNT_ID + "/images/v1");
+
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+        conn.setRequestProperty("Authorization", "Bearer " + API_TOKEN);
+
+        OutputStream httpConnOutputStream = conn.getOutputStream();
+        DataOutputStream request = new DataOutputStream(httpConnOutputStream);
+
+        request.writeBytes("--" + boundary + crlf);
+        request.writeBytes("Content-Disposition: form-data; name=\"url\"" + crlf);
+        request.writeBytes(crlf);
+        request.writeBytes(imageUrl + crlf);
+        request.writeBytes("--" + boundary + "--");
+        request.flush();
+        request.close();
+
+        int status = conn.getResponseCode();
+        if (status == HttpsURLConnection.HTTP_OK) {
+            InputStream is;
+            if (conn.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
+                is = conn.getInputStream();
+            } else {
+                is = conn.getErrorStream();
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            JsonObject jsonObject = JsonParser.parseString(br.lines().collect(Collectors.joining(""))).getAsJsonObject();
+
+            JsonArray errors = jsonObject.get("errors").getAsJsonArray();
+            if (!errors.isEmpty()) {
+                return null;
+            }
+
+            JsonObject result = jsonObject.get("result").getAsJsonObject();
+            JsonArray variants = result.getAsJsonArray("variants");
+            return variants.get(0).getAsString();
+        } else return null;
+    }
+
+    public static void deleteImage(String imageId) throws IOException {
+        URL url = new URL("https://api.cloudflare.com/client/v4/accounts/" + ACCOUNT_ID + "/images/v1/" + imageId);
+
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+        conn.setDoInput(true);
+        conn.setRequestMethod("DELETE");
+        conn.setRequestProperty("Authorization", "Bearer " + API_TOKEN);
+
+        int status = conn.getResponseCode();
+        if (status == HttpsURLConnection.HTTP_OK) {
+            InputStream is;
+            if (conn.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
+                is = conn.getInputStream();
+            } else {
+                is = conn.getErrorStream();
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            JsonObject jsonObject = JsonParser.parseString(br.lines().collect(Collectors.joining(""))).getAsJsonObject();
+
+            JsonArray errors = jsonObject.get("errors").getAsJsonArray();
+            if (!errors.isEmpty()) {
+                throw new IOException();
+            }
+        }
+    }
+
+    public static JsonArray listImage() throws IOException {
+        URL url = new URL("https://api.cloudflare.com/client/v4/accounts/" + ACCOUNT_ID + "/images/v2");
+
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+        conn.setDoInput(true);
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Authorization", "Bearer " + API_TOKEN);
+
+        int status = conn.getResponseCode();
+        if (status == HttpsURLConnection.HTTP_OK) {
+            InputStream is;
+            if (conn.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
+                is = conn.getInputStream();
+            } else {
+                is = conn.getErrorStream();
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            JsonObject jsonObject = JsonParser.parseString(br.lines().collect(Collectors.joining(""))).getAsJsonObject();
+
+            JsonArray errors = jsonObject.get("errors").getAsJsonArray();
+            if (!errors.isEmpty()) return null;
+
+            return jsonObject.get("result").getAsJsonObject().get("images").getAsJsonArray();
+        } else return null;
+    }
+}
