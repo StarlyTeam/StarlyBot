84df0c68e9787de302e6d683dd4c8c7d99c447b8
yejunho10 <hyjcompany30@naver.com>
2023-11-11 08:42:53 +0900

    feat: `PluginFile#serialize()`, `PluginFile#deserialize(Document)`

--- src/main/java/kr/starly/discordbot/repository/impl/mongo/MongoPluginFileRepository.java
+++ src/main/java/kr/starly/discordbot/repository/impl/mongo/MongoPluginFileRepository.java
@@ -1,20 +1,18 @@
 package kr.starly.discordbot.repository.impl.mongo;
 
 import com.mongodb.client.MongoCollection;
-import kr.starly.discordbot.configuration.DatabaseManager;
-import kr.starly.discordbot.entity.Plugin;
 import kr.starly.discordbot.entity.PluginFile;
 import kr.starly.discordbot.enums.MCVersion;
 import kr.starly.discordbot.repository.PluginFileRepository;
-import kr.starly.discordbot.service.PluginService;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.bson.Document;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 @AllArgsConstructor
 public class MongoPluginFileRepository implements PluginFileRepository {
 
@@ -27,8 +25,7 @@ public class MongoPluginFileRepository implements PluginFileRepository {
         filter.put("mcVersion", pluginFile.getMcVersion());
         filter.put("version", pluginFile.getVersion());
 
-        Document document = new Document(filter);
-        document.put("filePath", pluginFile.getFile().getPath());
+        Document document = pluginFile.serialize();
 
         if (collection.find(filter).first() != null) {
             collection.updateOne(filter, new Document("$set", document));
@@ -41,12 +38,9 @@ public class MongoPluginFileRepository implements PluginFileRepository {
     public List<PluginFile> findMany(String pluginNameEN) {
         Document filter = new Document("ENName", pluginNameEN);
 
-        List<PluginFile> pluginFiles = new ArrayList<>();
-        for (Document document : collection.find(filter)) {
-            pluginFiles.add(parsePluginFile(document));
-        }
-
-        return pluginFiles;
+        return collection.find(filter)
+                .map(PluginFile::deserialize)
+                .into(new ArrayList<>());
     }
 
     @Override
@@ -55,12 +49,9 @@ public class MongoPluginFileRepository implements PluginFileRepository {
         filter.put("ENName", pluginNameEN);
         filter.put("version", version);
 
-        List<PluginFile> pluginFiles = new ArrayList<>();
-        for (Document document : collection.find(filter)) {
-            pluginFiles.add(parsePluginFile(document));
-        }
-
-        return pluginFiles;
+        return collection.find(filter)
+                .map(PluginFile::deserialize)
+                .into(new ArrayList<>());
     }
 
     @Override
@@ -69,12 +60,9 @@ public class MongoPluginFileRepository implements PluginFileRepository {
         filter.put("ENName", pluginNameEN);
         filter.put("mcVersion", mcVersion);
 
-        List<PluginFile> pluginFiles = new ArrayList<>();
-        for (Document document : collection.find(filter)) {
-            pluginFiles.add(parsePluginFile(document));
-        }
-
-        return pluginFiles;
+        return collection.find(filter)
+                .map(PluginFile::deserialize)
+                .into(new ArrayList<>());
     }
 
     @Override
@@ -85,7 +73,7 @@ public class MongoPluginFileRepository implements PluginFileRepository {
         filter.put("version", version);
 
         Document document = collection.find(filter).first();
-        return parsePluginFile(document);
+        return PluginFile.deserialize(document);
     }
 
     @Override
@@ -121,17 +109,5 @@ public class MongoPluginFileRepository implements PluginFileRepository {
         filter.put("version", version);
 
         collection.deleteOne(filter);
-    }
-
-    private PluginFile parsePluginFile(Document document) {
-        if (document == null) return null;
-        PluginService pluginService = DatabaseManager.getPluginService();
-
-        File file = new File(document.getString("filePath"));
-        Plugin plugin = pluginService.getDataByENName(document.getString("ENName"));
-        MCVersion mcVersion = MCVersion.valueOf(document.getString("mcVersion"));
-        String version = document.getString("version");
-
-        return new PluginFile(file, plugin, mcVersion, version);
     }
 }
