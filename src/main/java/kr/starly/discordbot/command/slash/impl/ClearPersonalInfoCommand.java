0a475579f6973d9ae3771cfcaa260334e61baf52
호예준 <hyjcompany30@naver.com>
2023-12-25 01:18:01 +0900

    modify: message design

--- src/main/java/kr/starly/discordbot/command/slash/impl/ClearPersonalInfoCommand.java
+++ src/main/java/kr/starly/discordbot/command/slash/impl/ClearPersonalInfoCommand.java
@@ -2,13 +2,18 @@ package kr.starly.discordbot.command.slash.impl;
 
 import kr.starly.discordbot.command.slash.BotSlashCommand;
 import kr.starly.discordbot.command.slash.DiscordSlashCommand;
+import kr.starly.discordbot.configuration.ConfigProvider;
 import kr.starly.discordbot.configuration.DatabaseManager;
 import kr.starly.discordbot.service.*;
 import kr.starly.discordbot.util.security.PermissionUtil;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+
+import java.awt.*;
 
 @BotSlashCommand(
         command = "개인정보삭제",
@@ -19,6 +24,9 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
         optionRequired = {true}
 )
 public class ClearPersonalInfoCommand implements DiscordSlashCommand {
+
+    private final ConfigProvider configProvider = ConfigProvider.getInstance();
+    private final Color EMBED_COLOR_SUCCESS = Color.decode(configProvider.getString("EMBED_COLOR_SUCCESS"));
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
@@ -47,6 +55,11 @@ public class ClearPersonalInfoCommand implements DiscordSlashCommand {
         VerifyService verifyService = DatabaseManager.getVerifyService();
         verifyService.deleteDataByUserId(target.getIdLong());
 
-        event.reply("성공적으로 개인정보를 삭제했습니다. (블랙리스트, 경고 제외)").queue();
+        MessageEmbed embed = new EmbedBuilder()
+                .setColor(EMBED_COLOR_SUCCESS)
+                .setTitle("<a:success:1168266537262657626> 성공 | 개인정보삭제 <a:success:1168266537262657626>")
+                .setDescription("> **성공적으로 개인정보를 삭제했습니다. (블랙리스트, 경고 제외)**")
+                .build();
+        event.replyEmbeds(embed).queue();
     }
 }
